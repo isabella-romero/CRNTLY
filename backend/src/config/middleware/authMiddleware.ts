59af206d0ad8f8b9683eddf9f1274a9fc5530a37
nkamalu620 <nkamalu96@gmail.com>
2025-04-01 22:08:13 -0400

    updated code

--- backend/src/config/middleware/authMiddleware.ts
+++ backend/src/config/middleware/authMiddleware.ts
@@ -9,7 +9,13 @@ interface AuthRequest extends Request {
 }
 
 const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
-  const token = req.header("Authorization");
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  const token = authHeader.split(" ")[1];
 
   if (!token) return res.status(401).json({ msg: "No token, authorization denied" });
 
